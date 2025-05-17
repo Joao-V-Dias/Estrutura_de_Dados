312514cc54a4b32fb773973f525c3965767f2c93
joao <joaov.dias08@gmail.com>
2025-05-17 19:00:45 -0300

    Creating project Sequencia Aluno

---
+++ PilhaSequencialStructAlunoExe/PilhaSequencial.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "PilhaSequencial.h" //inclui os Prot�tipos
+
+//Definicao do tipo Pilha
+struct pilha{
+    int qtd;
+    struct aluno dados[MAX];
+};
+
+Pilha* cria_Pilha(){
+    Pilha
+}
+
